aa9b3ef05c91d44ae6f5dc203284023aa9a39fde
Marten Seemann <martenseemann@gmail.com>
2020-08-16 12:11:19 +0700

    use a tls.ConnectionState instead of redefining it
    This requires us to do a reinterpret-cast, as the tls.ConnectionState
    has an unexported ekm member.

--- conn.go
+++ conn.go
@@ -1484,7 +1484,7 @@ func (c *Conn) ConnectionState() ConnectionState {
 }
 
 func (c *Conn) connectionStateLocked() ConnectionState {
-	var state ConnectionState
+	var state connectionState
 	state.HandshakeComplete = c.handshakeComplete()
 	state.Version = c.vers
 	state.NegotiatedProtocol = c.clientProtocol
@@ -1508,7 +1508,7 @@ func (c *Conn) connectionStateLocked() ConnectionState {
 	} else {
 		state.ekm = c.ekm
 	}
-	return state
+	return toConnectionState(state)
 }
 
 // OCSPResponse returns the stapled OCSP response from the TLS server, if
